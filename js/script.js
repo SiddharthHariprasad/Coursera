f83268ff9ff80335d4df37573cb9dcf2ceabd1a9
Siddharth Hariprasad <siddhuhariprasad@gmail.com>
2020-08-13 21:55:41 +0530

    Dynamically Loading Menu Categories View

--- js/script.js
+++ js/script.js
@@ -24,10 +24,10 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 var dc = {};
 
 var homeHtml = "snippets/home-snippet.html";
-// var allCategoriesUrl =
-//   "https://davids-restaurant.herokuapp.com/categories.json";
-// var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-// var categoryHtml = "snippets/category-snippet.html";
+var allCategoriesUrl =
+  "https://davids-restaurant.herokuapp.com/categories.json";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
 // var menuItemsUrl =
 //   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 // var menuItemsTitleHtml = "snippets/menu-items-title.html";
@@ -46,14 +46,14 @@ var showLoading = function (selector) {
   insertHtml(selector, html);
 };
 
-// // Return substitute of '{{propName}}'
-// // with propValue in given 'string'
-// var insertProperty = function (string, propName, propValue) {
-//   var propToReplace = "{{" + propName + "}}";
-//   string = string
-//     .replace(new RegExp(propToReplace, "g"), propValue);
-//   return string;
-// }
+// Return substitute of '{{propName}}'
+// with propValue in given 'string'
+var insertProperty = function (string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  string = string
+    .replace(new RegExp(propToReplace, "g"), propValue);
+  return string;
+}
 
 // // Remove the class 'active' from home and switch to Menu button 
 // var switchMenuToActive = function () {
@@ -85,12 +85,12 @@ $ajaxUtils.sendGetRequest(
 });
 
 // // Load the menu categories view
-// dc.loadMenuCategories = function () {
-//   showLoading("#main-content");
-//   $ajaxUtils.sendGetRequest(
-//     allCategoriesUrl,
-//     buildAndShowCategoriesHTML);
-// };
+dc.loadMenuCategories = function () {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    buildAndShowCategoriesHTML);
+};
 
 
 // // Load the menu items view
@@ -103,56 +103,56 @@ $ajaxUtils.sendGetRequest(
 // };
 
 
-// // Builds HTML for the categories page based on the data
-// // from the server
-// function buildAndShowCategoriesHTML (categories) {
-//   // Load title snippet of categories page
-//   $ajaxUtils.sendGetRequest(
-//     categoriesTitleHtml,
-//     function (categoriesTitleHtml) {
-//       // Retrieve single category snippet
-//       $ajaxUtils.sendGetRequest(
-//         categoryHtml,
-//         function (categoryHtml) {
-//           var categoriesViewHtml =
-//             buildCategoriesViewHtml(categories,
-//                                     categoriesTitleHtml,
-//                                     categoryHtml);
-//           insertHtml("#main-content", categoriesViewHtml);
-//         },
-//         false);
-//     },
-//     false);
-// }
+// Builds HTML for the categories page based on the data
+// from the server
+function buildAndShowCategoriesHTML (categories) {
+  // Load title snippet of categories page
+  $ajaxUtils.sendGetRequest(
+    categoriesTitleHtml,
+    function (categoriesTitleHtml) {
+      // Retrieve single category snippet
+      $ajaxUtils.sendGetRequest(
+        categoryHtml,
+        function (categoryHtml) {
+          var categoriesViewHtml =
+            buildCategoriesViewHtml(categories,
+                                    categoriesTitleHtml,
+                                    categoryHtml);
+          insertHtml("#main-content", categoriesViewHtml);
+        },
+        false);
+    },
+    false);
+}
 
 
-// // Using categories data and snippets html
-// // build categories view HTML to be inserted into page
-// function buildCategoriesViewHtml(categories,
-//                                  categoriesTitleHtml,
-//                                  categoryHtml) {
+// Using categories data and snippets html
+// build categories view HTML to be inserted into page
+function buildCategoriesViewHtml(categories,
+                                 categoriesTitleHtml,
+                                 categoryHtml) {
 
-//   var finalHtml = categoriesTitleHtml;
-//   finalHtml += "<section class='row'>";
+  var finalHtml = categoriesTitleHtml;
+  finalHtml += "<section class='row'>";
 
-//   // Loop over categories
-//   for (var i = 0; i < categories.length; i++) {
-//     // Insert category values
-//     var html = categoryHtml;
-//     var name = "" + categories[i].name;
-//     var short_name = categories[i].short_name;
-//     html =
-//       insertProperty(html, "name", name);
-//     html =
-//       insertProperty(html,
-//                      "short_name",
-//                      short_name);
-//     finalHtml += html;
-//   }
+  // Loop over categories
+  for (var i = 0; i < categories.length; i++) {
+    // Insert category values
+    var html = categoryHtml;
+    var name = "" + categories[i].name;
+    var short_name = categories[i].short_name;
+    html =
+      insertProperty(html, "name", name);
+    html =
+      insertProperty(html,
+                     "short_name",
+                     short_name);
+    finalHtml += html;
+  }
 
-//   finalHtml += "</section>";
-//   return finalHtml;
-// }
+  finalHtml += "</section>";
+  return finalHtml;
+}
 
 
 
